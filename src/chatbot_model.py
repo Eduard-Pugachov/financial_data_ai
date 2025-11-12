06724c01e7dc498f8eb1a5b9a3f81acaff22f037
Eduard Pugachov <eddie.pugachov@gmail.com>
2025-11-11 23:52:52 -0500

    Updated directories, started intent generation

---
+++ src/chatbot_model.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Welcome to Equity - A Financial Chatbot")
+
+if 'counter' not in st.session_state:
+    st.session_state.counter = 0
+
+
+# Intent recognition (understanding what user wants)
+
+# Entity extraction (specific data points)
+
+# Response generation (creating appropriate answers)
+
+# Training data (patterns Equity learns from)
